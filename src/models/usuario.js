744fb928190f737dbc91b0ea3bc14f16d28b020a
ddouglss <ddouglss1999@gmail.com>
2025-07-11 17:10:53 -0300

    feat(models) implementando o models para usuario

---
+++ src/models/usuario.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true, select: false},
+    empresa: ({type: mongoose.Schema.Types.ObjectId, ref: 'Empresa'}),
+});
+
+UserSchema.pre('save', async function (next){
+   if(!this.isModified('password')) return next();
+   const salt = await bcrypt.genSalt(10);
+   this.password = await bcrypt.hash(this.password, salt);
+   next();
+});
+
+UserSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+};
+
+module.exports = mongoose.model('Usuario', UserSchema);
