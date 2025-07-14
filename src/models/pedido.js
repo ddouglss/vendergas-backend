18428f9b9c74a5f7f38b8643b12c1a7d1d1dccd3
ddouglss <ddouglss1999@gmail.com>
2025-07-13 23:18:34 -0300

    feat(model): criando entidade pedido

---
+++ src/models/pedido.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const PedidoProdutoSchema = require('./PedidoProduto');
+
+const PedidoSchema = new mongoose.Schema({
+    numero: { type: String, required: true },
+    cliente: { type: mongoose.Schema.Types.ObjectId, ref: 'Cliente', required: true },
+    empresa: { type: mongoose.Schema.Types.ObjectId, ref: 'Empresa', required: true },
+    observacao: { type: String },
+    data: { type: Date, default: Date.now },
+    produtos: [PedidoProdutoSchema]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Pedido', PedidoSchema);
