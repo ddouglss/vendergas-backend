b26fd526aa761faa40a01f8f7922f6188d4485f4
ddouglss <ddouglss1999@gmail.com>
2025-07-13 23:05:42 -0300

    feat(service) adicionando regras de negocio ao produto

---
+++ src/services/produtoService.js
@@ -0,0 +1,33 @@
+const Produto = require('src/models/produto');
+
+class ProdutoService {
+    async create(data) {
+        const produto = new Produto(data);
+        return await produto.save();
+    }
+
+    async getByEmpresa(empresaId) {
+        return await Produto.find({ empresa: empresaId });
+    }
+
+    async updateProduto(id, data) {
+        console.log('Tentando atualizar produto:', id, data);
+        const produto = await Produto.findByIdAndUpdate(id, data, { new: true });
+        if (!produto) {
+            console.error('Produto não encontrado no banco!');
+            throw new Error('Produto não encontrado');
+        }
+        return { message: 'Produto atualizado com sucesso', produto };
+    }
+
+
+    async deleteProduto(id) {
+        const result = await Produto.deleteOne({ _id: id });
+        if (result.deletedCount === 0) {
+            throw new Error('Produto não encontrado ou já removido');
+        }
+        return { message: 'Produto deletado com sucesso', deletedId: id };
+    }
+}
+
+module.exports = new ProdutoService();
