f0c3065fcac1b7ee3c8bd44d2cd0d9ef85fc4530
ddouglss <ddouglss1999@gmail.com>
2025-07-13 21:29:54 -0300

    feat(service):adicionando regras de negocio ao usuario

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./src/config/db');
-const baseRoutes = require('./routes/baseRoutes');
+const baseRoutes = require('./src/routes/baseRoutes');
 
 const createApp = async () => {
     const app = express();
-    app.use(cors());
+
+
+    app.use(cors({
+            origin: process.env.FRONTEND_URL,
+            credentials: true
+        }
+    ));
     app.use(express.json());
 
     const db = await connectDB();
--- src/models/usuario.js
+++ src/models/usuario.js
@@ -6,7 +6,13 @@ const UserSchema = new mongoose.Schema({
     nome: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true, select: false},
-    empresa: ({type: mongoose.Schema.Types.ObjectId, ref: 'Empresa'}),
+
+    role: {
+        type: String,
+        enum: ['admin', 'user','superadmin'],
+        default: 'user'
+    }
+
 });
 
 UserSchema.pre('save', async function (next){
@@ -21,7 +27,8 @@ UserSchema.methods.comparePassword = async function (password) {
 };
 
 UserSchema.methods.generateAuthToken = function () {
-    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+    return jwt.sign({id: this._id, role: this.role},
+        process.env.JWT_SECRET, {expiresIn: '1h'});
 };
 
 module.exports = mongoose.model('Usuario', UserSchema);
