db4d97fba9d2b809ba46344ab0a9c781a5d1d1d2
ddouglss <ddouglss1999@gmail.com>
2025-07-13 22:16:03 -0300

    feat(models): criando entidade empresa

---
+++ src/models/empresa.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const EmpresaSchema = new mongoose.Schema({
+    nomeFantasia: { type: String, required: true },
+    razaoSocial: { type: String, required: true },
+    cnpj: { type: String, required: true, unique: true },
+    usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'usuario', required: true },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Empresa', EmpresaSchema);
