231c6c1393fb496cc387e6106bbcf510f4b49c3b
ddouglss <ddouglss1999@gmail.com>
2025-07-14 23:27:59 -0300

    Deploy em produção para o Railway

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,18 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI || 'mongodb://mongo:27017/desafio-db', {
+        const mongoUri = process.env.MONGO_URI
+            || 'mongodb://localhost:27017/desafio-db'
+            || 'mongodb://mongo:27017/desafio-db';
+
+        console.log('Conectando ao MongoDB em:', mongoUri);
+
+        await mongoose.connect(mongoUri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log(`✅ MongoDB conectado com sucesso!`);
+
+        console.log('✅ MongoDB conectado com sucesso!');
         return mongoose.connection;
     } catch (err) {
         console.error('❌ Falha na conexão com MongoDB:', err.message);
